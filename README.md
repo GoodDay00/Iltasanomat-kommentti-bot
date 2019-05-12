71e0774d61f1da8f6fc9c0d4b22aa48be53fd890
Good_Day <ville.2000.vhk@gmail.com>
2019-05-12 20:30:59 +0300

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Iltasanomat-kommentti-bot
+Komentti botti Iltasanomat sivustolle, jolla voit antaa haluamallesi kommentille tykkäyksiä niin paljon kuin haluat.
---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+import json
+
+articleID = None
+def getComments(URL):
+    global articleID
+    try:
+        splitted =URL.split("-")
+        id = splitted[1].split(".")
+        articleID = id[0]
+        commentURL = f"https://www.is.fi/rest/articles/{articleID}/comments?offset=0&perPage=1000"
+        comments = requests.get(commentURL)
+        return json.loads(comments.content)
+
+    except Exception as e:
+        print(f"Error building the link!{str(e)}")
+
+def findCommentId(commentList,name):
+    try:
+        for user in commentList['list']:
+            if name == user['userNickname']:
+                print("Users comment found!")
+                return user['id']
+        print("kommenttia ei löytynyt tuolla nimimerkillä")
+    except Exception as e:
+        print(f"error: kommenttia ei löydy! {str(e)}")
+
+def sendVotes(amount, commentID):
+    global articleID
+    try:
+        body = {"voteType":"upvote"}
+        headers = {'Content-type': 'application/json; charset=UTF-8'}
+        for i in range(int(amount)):
+            r = requests.post(f"https://www.is.fi/rest/articles/{articleID}/comments/{commentID}/votes", json=body, headers=headers)
+            if r.status_code != 200:
+                print("Ei toimi")
+
+    except Exception as e:
+        print(f"Ei voi äänestää :( {str(e)}")
+
+
+URL = input("Anna uutisen linkki jossa on kommenttisi: ")
+if URL == "q":
+    exit()
+comments = getComments(URL)
+commentName = input("Anna kommentin käyttäjänimi: ")
+if commentName == "q":
+    exit()
+commentID = findCommentId(comments, commentName)
+voteAmount = input("Anna tykkäysten määrä: ")
+if voteAmount == "q":
+    exit()
+sendVotes(voteAmount, commentID)
